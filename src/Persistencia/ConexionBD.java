1240996e5be3ed61e20c60a173ce06c5445d3ff8
Ale <Ale@192.168.56.1>
2017-11-07 23:55:18 -0300

    Merge origin/Tarea2 into Tarea2
    Conflicts:
    	configuracion.properties
    	nbproject/project.properties
    	src/Presentacion/menu.java
    	src/Servicios/PLista.java

--- src/Persistencia/ConexionBD.java
+++ src/Persistencia/ConexionBD.java
@@ -12,7 +12,7 @@ import javax.swing.JOptionPane;
 
 public class ConexionBD {
 
-    private final String host = "localhost";
+    private final String host = "127.0.0.1";
     private final String port = "3306";
     private final String db = "espotify";
     private final String user = "root";
@@ -55,66 +55,6 @@ public class ConexionBD {
                 ex.printStackTrace();
                 Logger.getLogger(ConexionBD.class.getName()).log(Level.SEVERE, null, ex);
             }
-        }
-    }
-
-    //----------------------------------------------  arme otro tipo de conexion e hice funciones para simplificar la devoluciopnde datos-----------------
-    //-----------------------------------------    //-------------------------------------------------------------------------------------------------------------------------------------------------
-    public boolean conectar() // funcion que conecta al a base de ddatos con los parametros que estan arriba
-    {
-
-        this.url = "jdbc:mysql://" + host + "/" + db;
-
-        try {
-            //Class.forName("org.gjt.mm.mysql.Driver");
-            Class.forName("com.mysql.jdbc.Driver");
-            this.Link = DriverManager.getConnection(this.url, this.user, this.pass);
-            if (this.Link != null) {
-                this.conectado = true;
-                return true;
-            }
-        } catch (SQLException ex) {
-
-            JOptionPane.showMessageDialog(null, "ERROR DE CONEXION A: " + this.url + "  Configure correctamente los datos del servidor, si el problema persiste se recomienda reiniciar el servidor y el programa", "Problema de conexion", JOptionPane.INFORMATION_MESSAGE);
-
-        } catch (ClassNotFoundException ex) {
-            System.out.println(ex);
-        }
-        return false;
-    }
-
-    public void desconectar()// cierra la conexion
-    {
-        this.Link = null;
-    }
-
-    public Connection GetConection() // devuele la conexion
-    {
-        return this.Link;
-    }
-
-    public boolean Ejecutar(String SQL)// funcion que ejecuta unaconsulta sql , que se pasa como string 
-    {
-        try {
-            PreparedStatement Query = this.GetConection().prepareStatement(SQL);
-            Query.execute();
-            return true;
-        } catch (SQLException ex) {
-            //System.out.println("ERROR: "+ex);
-            return false;
-        }
-
-    }
-
-    public ResultSet Consultar(String SQL)//funcion que ejecuta consulta sql , que se pasa como string, yella devuelve un resulet con los datos 
-    {
-        try {
-            PreparedStatement Query = this.GetConection().prepareStatement(SQL);
-            ResultSet Resultado = Query.executeQuery();
-            return Resultado;
-        } catch (SQLException ex) {
-            System.out.println("ERROR: " + ex);
-            return null;
         }
     }
 
